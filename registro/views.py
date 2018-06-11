d9c7f0e15eadaa6f0abbd185eb3a32c77b2e5cf0
aosorio <argenisosorio580@hotmail.com>
2018-06-11 09:56:12 -0400

    Agregado método que valida si un usuario tiene permisos para ver la lista de proyectos

--- registro/views.py
+++ registro/views.py
@@ -36,6 +36,28 @@ class Consultar_proyecto(ListView):
     """
     model = Proyecto
 
+    def get(self, request, *args, **kwargs):
+        """
+        Método que valida si un usuario tiene permisos para ver la lista de proyectos.
+        """
+        self.object_list = self.get_queryset()
+        allow_empty = self.get_allow_empty()
+        if not allow_empty:
+            if (self.get_paginate_by(self.object_list) is not None
+                    and hasattr(self.object_list, 'exists')):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = len(self.object_list) == 0
+            if is_empty:
+                raise Http404(_("Empty list and '%(class_name)s.allow_empty' is False.")
+                        % {'class_name': self.__class__.__name__})
+        context = self.get_context_data()
+        if request.user.is_staff:
+            return self.render_to_response(context)
+        else:
+            messages_alert = ['No tiene permisos para editar el reporte']
+            return render_to_response("registro/index.html",{'messages_alert': messages_alert}, context_instance=RequestContext(request))
+
 
 class Registrar_proyecto(SuccessMessageMixin,CreateView):
     """
