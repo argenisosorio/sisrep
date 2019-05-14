3eda3821602ebce3ceb4bc664b09c0b200cd3beb
Argenis Osorio <argenisosorio580@hotmail.com>
2019-05-14 08:33:28 -0400

    Agregando el filtro para que cada cara visible pueda ver solo sus reportes

--- registro/views.py
+++ registro/views.py
@@ -360,6 +360,34 @@ class Consultar_reporte(ListView):
     """
     model = Reporte
 
+    def get_queryset(self):
+        """
+        Return the list of items for this view.
+        The return value must be an iterable and may be an instance of
+        `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+        """
+        if self.queryset is not None:
+            queryset = self.queryset
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all()
+        elif self.model is not None:
+            queryset = self.model._default_manager.all()
+        else:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset()." % {
+                    'cls': self.__class__.__name__
+                }
+            )
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, six.string_types):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+        queryset = queryset.filter(autor=str(self.request.user))
+        return queryset
+
 
 class Registrar_reporte(SuccessMessageMixin,CreateView):
     """
