30535b51ab3b091be15951efcd54454d154406c6
Argenis Osorio <argenisosorio580@hotmail.com>
2019-11-19 12:34:56 -0400

    Arreglando detalles en la nueva implementación del reseteo de contraseña por correo

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,40 +6,26 @@ from .views import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 
-urlpatterns = patterns('',
-    url(r'^login/$', LoginView.as_view(), name = "login"),
-    url(r'logout/$', views.Logout.as_view(), name='logout'),
-    url(r'^register$', views.RegisterUser.as_view(), name='register'),
-    url(r'^list_users/$', login_required(views.ListUsers.as_view()), name='list_users'),
-    url(r'^cambiar-contrasena/$', login_required(views.change_password), name='change_password'),
-    url(r'^edit_user/(?P<pk>\d+)$', login_required(views.EditUser.as_view()), name='edit_user'),
-    url(r'^delete_user/(?P<pk>\d+)$', login_required(views.DeleteUser.as_view()), name='delete_user'),
-    url(r'^user_change_password/(?P<pk>\d+)$', login_required(views.user_change_password), name='user_change_password'),
-    
-    url(r'^resetpassword/$', 'django.contrib.auth.views.password_reset', 
-        {'post_reset_redirect' : 'passwordsent/', 'template_name' : 'usuarios/password_reset_form.html',
-        'email_template_name' : 'usuarios/password_reset_email.html'}, name='password_reset'),
-        
-    url(r'^resetpassword/passwordsent/$', 'django.contrib.auth.views.password_reset_done',
-        {'template_name' : 'usuarios/password_reset_done.html'}, name='password_reset_done'),
-
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$', 
-        'django.contrib.auth.views.password_reset_confirm', 
-        {'post_reset_redirect' : reverse_lazy('usuarios:password_reset_complete'), 'template_name' : 'usuarios/password_reset_confirm.html'},
+urlpatterns=patterns('',
+    url(r'^login/$',LoginView.as_view(),name="login"),
+    url(r'logout/$',views.Logout.as_view(),name='logout'),
+    url(r'^register$',views.RegisterUser.as_view(),name='register'),
+    url(r'^list_users/$',login_required(views.ListUsers.as_view()),name='list_users'),
+    url(r'^cambiar-contrasena/$',login_required(views.change_password),name='change_password'),
+    url(r'^edit_user/(?P<pk>\d+)$',login_required(views.EditUser.as_view()),name='edit_user'),
+    url(r'^delete_user/(?P<pk>\d+)$',login_required(views.DeleteUser.as_view()),name='delete_user'),
+    url(r'^user_change_password/(?P<pk>\d+)$',login_required(views.user_change_password),name='user_change_password'),
+    # Urls para el restablecimiento de contraseña por correo.
+    url(r'^resetpassword/$', 'django.contrib.auth.views.password_reset',
+        {'post_reset_redirect':'passwordsent/','template_name':'usuarios/password_reset_form.html',
+        'email_template_name':'usuarios/password_reset_email.html'},name='password_reset'),
+    url(r'^resetpassword/passwordsent/$','django.contrib.auth.views.password_reset_done',
+        {'template_name':'usuarios/password_reset_done.html'},name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'post_reset_redirect':reverse_lazy('usuarios:password_reset_complete'),'template_name':'usuarios/password_reset_confirm.html'},
         name='password_reset_confirm'),
-
-    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', 
-        {'template_name' : 'usuarios/password_reset_complete.html'},
+    url(r'^reset/done/$','django.contrib.auth.views.password_reset_complete',
+        {'template_name':'usuarios/password_reset_complete.html'},
         name='password_reset_complete'),
-    
-    #url(r'^resetpassword/$', 'django.contrib.auth.views.password_reset',  
-    #    {'post_reset_redirect' : 'password_reset_done/'}, name='password_reset'),
-
-    #url(r'^password_reset_done/$','django.contrib.auth.views.password_reset_done', 
-    #    {'post_reset_redirect' : 'password_reset_confirm/'}, name='password_reset_done'),
-
-    #url(r'^password_reset_confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    #    'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-
-    #url(r'^password_reset_complete/', auth_views.password_reset_complete, name='password_reset_complete')
 )
