55c45f77728c73370250e5d1a36166040cc7377c
Argenis Osorio <argenisosorio580@hotmail.com>
2025-09-18 21:03:59 -0400

    feat(Image): Actualizada imagen del cintillo de gobierno en los reportes PDF

--- reporte_mensual_proyecto/urls.py
+++ reporte_mensual_proyecto/urls.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import JsonResponse
 
 
 urlpatterns = [
@@ -10,4 +13,10 @@ urlpatterns = [
     url(r'^safet/', include('safet.urls', namespace='safet')),
     url(r'^bitacora/', include('bitacora.urls', namespace='bitacora')),
     url(r'^admin/', include(admin.site.urls)),
+    # Devolver la versión del software.
+    url(r'^version/$',
+        lambda request: JsonResponse({
+            'version': open(os.path.join(settings.BASE_DIR, 'VERSION.txt')).read().strip()
+        }),
+        name='version'),
 ]
